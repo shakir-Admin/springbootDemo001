ccd0daa2b7fb6a41f361f0cee727678a054e3c74
xushenke <xushenke@163.com>
2025-08-08 15:32:14 +0800

    master v2 2025/08/08 15:31 by shenke

--- src/main/java/com/example/demo/config/GitTest.java
+++ src/main/java/com/example/demo/config/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fix 2");
         System.out.println("hello master v1");
+        System.out.println("hello master v2");
+
     }
 }

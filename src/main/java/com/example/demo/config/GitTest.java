eed1a247e0bfb393280d3febf800a34a9517887c
xushenke <xushenke@163.com>
2025-08-08 15:53:15 +0800

    master v4 2025/08/08 15:53 by shenke

--- src/main/java/com/example/demo/config/GitTest.java
+++ src/main/java/com/example/demo/config/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello master v1");
         System.out.println("hello master v2");
         System.out.println("hello master v3");
+        System.out.println("hello master v4");
         System.out.println("pull test v1");
     }
 }
